82ec9ec5a8ce1239c875e51bacb0eca588963ad3
Zhao Tang <tangzhao1999@hotmail.com>
2025-01-13 19:06:04 +0800

    implementing action model

--- cyber/models/action/action_model.py
+++ cyber/models/action/action_model.py
@@ -1,2 +1,22 @@
 # action_model.py
 # Description: This file contains the ActionModel class, which is responsible for handling the action model.
+
+import numpy as np
+
+from cyber.models import CyberModule
+
+
+class ActionModel(CyberModule):
+    def __init__(self, backbone: str, objective: str):
+        super().__init__()
+
+
+class CyberAgent:
+    def __init__(self, model: ActionModel):
+        self.model = model
+
+    def get_action(self, obs: np.ndarray) -> np.ndarray:
+        return self.model.get_action(obs)
+
+    # def train(self, obs: np.ndarray, action: np.ndarray, reward: float, next_obs: np.ndarray) -> None:
+    #     self.model.train(obs, action, reward, next_obs)
