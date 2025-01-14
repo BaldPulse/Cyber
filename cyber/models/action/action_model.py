d98e27517b717f10a264e16ea6bbf30377920c01
Zhao Tang <tangzhao1999@hotmail.com>
2025-01-14 15:10:31 +0800

    implemented register and delete domain methods

--- cyber/models/action/action_model.py
+++ cyber/models/action/action_model.py
@@ -1,6 +1,9 @@
 # action_model.py
 # Description: This file contains the ActionModel class, which is responsible for handling the action model.
 
+# The ActionModel class is heavily inspired by the HPT project https://github.com/liruiw/HPT without
+# using the HPT codebase.
+
 import numpy as np
 
 from torch import nn
@@ -30,6 +33,12 @@ class ActionModel(CyberModule):
     module_registry: nn.ModuleDict = nn.ModuleDict()  # shared registry for all modules. this makes sharing modules easier
     module_info: ClassVar[dict[str, tuple[str, int, int, List[str]]]] = {}  # module_name: (classname, num_params, num_trainable_params, used_in)
 
+    """
+    The advent of transformers has made it easy to handle highly heterogeneous data.
+    This structure assumes that the trunk has the ability to handle any kind of data.
+    """
+    domain_registry: ClassVar[dict[str, tuple[str, str, str]]] = {}  # registry holding the stem, trunk, and head for each domain
+
     def register_module(
         self,
         module: nn.Module,
@@ -49,6 +58,8 @@ class ActionModel(CyberModule):
         """
         if name is None:
             name = module.__class__.__name__
+        if "<unregistered>" in name:
+            raise ValueError("<unregistered> is a reserved keyword. Please choose another name >:-(")
         # check if the module is already registered
         if name in self.module_registry:
             raise ValueError(f"Module with name {name} is already registered. Delete it first.")
@@ -73,7 +84,7 @@ class ActionModel(CyberModule):
         if name not in self.module_registry:
             raise ValueError(f"Module with name {name} is not registered.")
         del self.module_registry[name]
-        del self.module_info[name]
+        del self.module_info[name]  # TODO: mark domains that use the deleted module as "<unregistered>"
 
     def print_module_info(self) -> None:
         """
@@ -84,6 +95,58 @@ class ActionModel(CyberModule):
         for name, info in self.module_info.items():
             table.add_row([name, *info])
         print(table)  # noqa: T201
+
+    def register_domain(self, domain: str, stem: str, trunk: str, head: str) -> None:
+        """
+        Register the stem, trunk, and head for a domain.
+
+        Args:
+            domain (str): the domain to register
+            stem (str): the stem to use
+            trunk (str): the trunk to use
+            head (str): the head to use
+
+        Raises:
+            ValueError: if the stem is not registered
+            ValueError: if the trunk is not registered
+            ValueError: if the head is not registered
+            ValueError: if the domain is already registered
+        """
+        # for each of the components, check if they are registered
+        if stem not in self.module_registry:
+            raise ValueError(f"Stem {stem} is not registered.")
+        if trunk not in self.module_registry:
+            raise ValueError(f"Trunk {trunk} is not registered.")
+        if head not in self.module_registry:
+            raise ValueError(f"Head {head} is not registered.")
+
+        # register the domain
+        if domain in self.domain_registry:
+            raise ValueError(f"Domain {domain} is already registered.")
+        self.domain_registry[domain] = (stem, trunk, head)
+        # update the module info
+        self.module_info[stem][3].append(domain + ".stem")
+        self.module_info[trunk][3].append(domain + ".trunk")
+        self.module_info[head][3].append(domain + ".head")
+
+    def delete_domain(self, domain: str) -> None:
+        """
+        Delete the domain from the domain registry.
+
+        Args:
+            domain (str): the domain to delete
+
+        Raises:
+            ValueError: if the domain is not registered
+        """
+        if domain not in self.domain_registry:
+            raise ValueError(f"Domain {domain} is not registered.")
+        stem, trunk, head = self.domain_registry[domain]
+        del self.domain_registry[domain]
+        # update the module info
+        self.module_info[stem][3].remove(domain + ".stem")
+        self.module_info[trunk][3].remove(domain + ".trunk")
+        self.module_info[head][3].remove(domain + ".head")
 
 
 class CyberAgent:
